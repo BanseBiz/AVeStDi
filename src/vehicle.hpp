a9e15d39f24ed374e5a3780f216d98e75a0ca1dc
Marvin Banse <marvin@banse.biz>
2023-11-09 00:46:24 +0100

    define vehicle setters

--- src/vehicle.hpp
+++ src/vehicle.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <time.h>
+
 class Vehicle
 {
     public:
@@ -13,6 +15,13 @@ class Vehicle
         Y = 0x2,
         Z = 0x3
     };
+
+    int setPosition(double, double, double, time_t);
+    int setOrientation(double, double, double, time_t);
+    int setVelocity(double, double, double, time_t);
+    int setRotation(double, double, double, time_t);
+    int setAcceleration(double, double, double, time_t);
+    int setAngularAcceleration(double, double, double, time_t);
     
     private:
     double _position[3];                // latitude, longitude, altitude
@@ -21,4 +30,5 @@ class Vehicle
     double _rotation[3];                // radians / second
     double _acceleration[3];            // meters per square second
     double _angular_acceleration[3];    // radians per square second
+    time_t _recent_update;
 };
